579a850522a3224f886cdeaf5897aa4e7edceead
Eduardo Vasconcelos <c.eduvasconcelos@gmail.com>
2024-04-08 00:02:26 -0300

    Testing database

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     dd(User::al());
     return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
